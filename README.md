94726118c094817607796f990489c34fc954200e
小明晚安了 <82751046+Tcm00@users.noreply.github.com>
2023-02-06 15:32:38 +0800

    米雪冰城后台

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# order_shop_back
+# 项目介绍
+这是与仿蜜雪冰城点餐系统配套的后台系统，基于vue开发，目前完成了用户查询，首页轮博图管理，商品管理等功能。
+项目部署在[http://xyxzxl.com/h5/shopnowsystem](http://xyxzxl.com/h5/shopnowsystem)
+
+## 初始化
+```
+npm install
+```
+
+## 开发编译
+```
+npm run serve
+```
+
+## 打包
+```
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+html {
+    font-size: 1.4vw;
+}
+#app {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+}
+</style>
